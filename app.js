0de24a645a5a7556460f795ab1043626c8a8cd21
Alex-mp4 <alexander.heino@elev.ga.ntig.se>
2023-01-20 11:10:26 +0100

    🚀 i did stuff (tutoriaol comaple te)

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const nunjucks = require('nunjucks');
 const app = express();
+const bodyParser = require('body-parser')
 const port = 3000;
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 
 const indexRouter = require('./routes/index');
 
